f4fba8bae5446c2cbb6b83c5a5b84d4ba8d31035
bailwalVipin08 <vipinbailwal05@gmail.com>
2020-04-15 16:48:53 +0530

    added logging with morgan

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const courseRouter = require('./routes/courseRoutes')
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json()) // express built in middleware to work with json req/res
 app.use('/api/courses', courseRouter)
