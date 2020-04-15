8393b31bd1cd2ed37ea7ef52735ab2f6e4fd99a8
bailwalVipin08 <vipinbailwal05@gmail.com>
2020-04-15 21:09:34 +0530

    added 404 error handler

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json()) // express built in middleware to work with json req/res
 app.use('/api/courses', courseRouter)
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Cannot find ${req.originalUrl} on this server!`,
+  })
+})
+
 module.exports = app
